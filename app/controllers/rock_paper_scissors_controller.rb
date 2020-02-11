82adb120a47ac48729ec6563ec0f31ac4fbb6062
Vladislav Volkov <uladzislau.volkau@azati.com>
2020-02-11 18:32:57 +0300

    normalize play method to show normal result

--- app/controllers/rock_paper_scissors_controller.rb
+++ app/controllers/rock_paper_scissors_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class RockPaperScissorsController < ApplicationController
-  BETS = %w[rock paper scissors].freeze
-
   def index
     @context[:bets] = RockPaperScissorsService::BETS
   end
@@ -10,10 +8,12 @@ class RockPaperScissorsController < ApplicationController
   def play
     game_service = RockPaperScissorsService.new(params[:bet])
 
-    @context[:result] = game_service.play_game.result
+    game_service.play_game
+
+    @context[:result] = game_service.result
     @context[:computer_choice] = game_service.computer_choice
     @context[:human_choice] = game_service.human_choice
 
-    render "game_result.js.erb"
+    render "result.js.erb"
   end
 end
