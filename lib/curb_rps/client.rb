8ae06cf5cc2187b0ac3611d8a4561a71ce1efe9f
Vladislav Volkov <uladzislau.volkau@azati.com>
2020-02-11 19:04:24 +0300

    improve client

--- lib/curb_rps/client.rb
+++ lib/curb_rps/client.rb
@@ -2,8 +2,11 @@
 
 module CurbRps
   class Client
+    API_URL = "https://5eddt4q9dk.execute-api.us-east-1.amazonaws.com/rps-stage/throw"
+    TIMEOUT = 3
+
     def load_throw
-      response = HTTP.get("https://5eddt4q9dk.execute-api.us-east-1.amazonaws.com/rps-stage/throw")
+      response = HTTP.timeout(connect: TIMEOUT, read: TIMEOUT).get(API_URL)
 
       CurbRps::Result.new(response)
     end
